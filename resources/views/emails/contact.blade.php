77865467e6998aee26f6a22e39dc74458d935b12
Elias Soares <esoares@iget.com.br>
2016-04-27 11:48:58 -0300

    Improve contact view

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,10 +1,10 @@
 <p><strong>Nome:</strong> {!! $data['name'] !!}</p>
 <p><strong>Email:</strong> {!! $data['email'] !!}</p>
-@if (array_key_exists('phone', $data))
+@if (array_key_exists('phone', $data) && strlen($data['phone']))
     <p><strong>Telefone:</strong> {!! $data['phone'] !!}</p>
 @endif
 
 <p><strong>Assunto:</strong> {!! $data['subject'] !!}</p>
 
-<p>Mensagem:</p>
+<p><strong>Mensagem:</strong></p>
 <p>{!! $data['message'] !!}</p>
